e69246f8e5c894eacb8fc1cd8ffae9ba41c3546f
mirzawajihali <mirzawajihali00@gmail.com>
2025-08-30 19:30:16 +0600

    feat : toggle using dark and light theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import { Button } from './components/ui/button'
+import { ModeToggle } from './layout/ModeToggler'
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
    <div>
      <h1>Hello World</h1>
      <Button>Click Me mmuhhahaha  jongi   jaaaaaaaaaaaaaaaaaaha</Button>
+     <ModeToggle></ModeToggle>
    </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,18 +4,17 @@ import "./index.css";
 import { RouterProvider } from "react-router";
 
 import { Provider as ReduxProvider } from "react-redux";
-import { store } from "./redux/store.ts";
 
-
-import router from "./routes/index.tsx";
 import { Toaster } from "sonner";
 import ThemeProvider from "./providers/theme-provider.tsx";
+import { store } from "./redux/store.ts";
+import router from "./routes/index.tsx";
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ReduxProvider store={store}>
-      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <RouterProvider router={router} />
         <Toaster/>
       </ThemeProvider>
