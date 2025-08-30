23e1eb8d11f5eaefc35d875b223c359a8a1edf85
mirzawajihali <mirzawajihali00@gmail.com>
2025-08-30 19:17:06 +0600

    feat : theme provider, redux and axios setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./redux/store.ts";
+
+
+import router from "./routes/index.tsx";
+import { Toaster } from "sonner";
+import ThemeProvider from "./providers/theme-provider.tsx";
+
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ReduxProvider store={store}>
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+        <Toaster/>
+      </ThemeProvider>
+    </ReduxProvider>
+  </React.StrictMode>
+);
---
+++ src/redux/bassApi.ts
@@ -0,0 +1,10 @@
+import { createApi } from "@reduxjs/toolkit/query/react";
+import axiosBaseQuery from "./AxiosBaseQuery";
+
+
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: axiosBaseQuery(),
+  tagTypes : ["USER", "TOUR", "DIVISION", "BOOKING"],
+  endpoints: () => ({}),
+});
